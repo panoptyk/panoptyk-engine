de8a9f49ddd20cc70bc3fecd0f7333ab2186e038
scm2217 <smendonc@gmail.com>
2019-08-11 20:23:53 -0700

    fixed app path

--- src/server/app.ts
+++ src/server/app.ts
@@ -5,14 +5,14 @@ const app: express.Application = express();
 // Anything to do with the express app server of panoptyk
 // Mostly includes creating paths to certain html/js client files
 
-app.use("/public/game", express.static(__dirname + "/public/game"));
+app.use("/public/game", express.static(process.cwd() + "/public/game"));
 
 app.get("/test", function(req, res) {
-  res.sendFile(__dirname + "/public/test.html");
+  res.sendFile(process.cwd() + "/public/test.html");
 });
 
 app.get("/game", function(req, res) {
-  res.sendFile(__dirname + "/public/game/game.html");
+  res.sendFile(process.cwd() + "/public/game/game.html");
 });
 
 export default app;
