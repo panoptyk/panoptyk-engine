f3806aad5b99a22df81d567c11924150a0ad0fbb
scm2217 <smendonc@gmail.com>
2019-10-03 22:48:24 -0700

    dummy bot optimization

--- example_bots/dummy.ts
+++ example_bots/dummy.ts
@@ -5,28 +5,24 @@ import { getPanoptykDatetime } from "../src/core/utilities/util";
 const username = process.argv.length >= 3 ? process.argv[2] : "dummy";
 const password = process.argv.length > 3 ? process.argv[3] : "password";
 
-function pickRoom(): number {
+function pickRoom(): Room {
     const adjacents = ClientAPI.playerAgent.room.getAdjacentRooms();
     const next = Math.floor(Math.random() * Math.floor(adjacents.length));
-    return adjacents[next].id;
+    return adjacents[next];
 }
 
 async function main() {
+    ClientAPI.init();
     await ClientAPI.login(username, password).then(res => {
         console.log("Login success! " + ClientAPI.playerAgent);
     }).catch(err => {
         throw new Error("Login fail!");
     });
     // aimlessly switch rooms every 3 seconds
-    let lastMove = getPanoptykDatetime();
     while (true) {
-        if (getPanoptykDatetime() - lastMove >= 3) {
-            await ClientAPI.moveToRoom(pickRoom()).then(res => {
-                lastMove = getPanoptykDatetime();
-            }).catch(err => {
-                throw new Error(err);
-            });
-        }
+        // tslint:disable-next-line: ban
+        await new Promise(javascriptIsFun => setTimeout(javascriptIsFun, 3000));
+        await ClientAPI.moveToRoom(pickRoom());
     }
 }
 
