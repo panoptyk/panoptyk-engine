34faccc9899c1088380a2aed146c9e366ffe1459
scm2217 <smendonc@gmail.com>
2019-10-03 22:46:23 -0700

    removal of old files

--- src/server/communication/socket_output.ts
+++
@@ -1,9 +0,0 @@
-import { logger } from "../../core/utilities/logger";
-import { Agent, IDObject } from "../../core/models/index";
-
-export const sendUpdate = function(updates: Map<Agent, Set<IDObject>>) {
-    for (const [agent, models] of updates) {
-        logger.log("Sending model updates to " + Agent.name, 1);
-        agent.socket.emit("model-updates", JSON.stringify(models));
-    }
-};
