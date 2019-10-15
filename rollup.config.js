933f156b3bbf6afb9432ec80c0c0f5283e5be4a3
Mitchell Miller <mitchkm12@gmail.com>
2019-10-15 05:25:24 -0700

    changes rollup to esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        output: "./dist",
-        format: 'cjs'
-      },
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-      "fs",
-      "http"
-    ],plugins: [
-      typescript({
-        typescript: require('typescript'),
-      }),
-    ],
-  }
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      output: "./dist",
+      format: "esm"
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    "fs",
+    "http"
+  ],
+  plugins: [
+    typescript({
+      typescript: require("typescript")
+    })
+  ]
+};
