41602b46282004c1b887083ee297b07b09c28ffb
Mitchell Miller <mitchkm12@gmail.com>
2020-02-06 00:02:38 -0800

    adds missing export for interface ValidationResult

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 export * from "./models/index";
 export *  from "./models/action/index";
+export { ValidationResult } from "./models/validate";
 export { ClientAPI } from "./clientAPI";
 export { logger, LOG } from "./utilities/logger";
 export { getPanoptykDatetime, formatPanoptykDatetime, randomInt } from "./utilities/util";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./models/index";
 export *  from "./models/action/index";
+export { ValidationResult } from "./models/validate";
 export { Server } from "./server";
 export { ClientAPI } from "./clientAPI";
 export { logger, LOG } from "./utilities/logger";
