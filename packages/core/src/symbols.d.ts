fd2fc9de9e54089497e8f346fc2f540609e5225e
Jace7Valentine <mitchkm12@gmail.com>
2021-10-27 21:32:17 -0700

    adds simple alias for numbers to indicate which model and ID number is

---
+++ packages/core/src/symbols.d.ts
@@ -0,0 +1,10 @@
+//Model type aliases
+type AgentID = number;
+type ConversationID = number;
+type FactionID = number;
+type InfoID = number;
+type ItemID = number;
+type QuestID = number;
+type RecipeID = number;
+type RoomID = number;
+type TradeID = number;
